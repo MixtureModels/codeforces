5d9daee3a8923b8797db3efc515ad76619d81fb3
mixturemodels <sl@DESKTOP-961IQMT>
2024-09-21 17:38:59 -0700

    first successful submission, 1873F

--- 1300/1873F/1873F.cpp
+++ 1300/1873F/1873F.cpp
@@ -1,1 +1,47 @@
 #include <bits/stdc++.h>
+#include <iostream>
+using namespace std;
+
+int main() {
+    int t;
+    cin >> t;
+
+    int n; int k; int mx_len;
+    int cnt; int l;
+
+    while (t--) {
+
+        cin >> n; cin >> k;
+
+        vector<long long> fr(n); vector<long long> ht(n);
+
+        // store the fruits and the heights
+        for (int i = 0; i < n; ++i) cin >> fr[i];
+        for (int i = 0; i < n; ++i) cin >> ht[i];
+        
+        // two-pointer sliding window
+        cnt = fr[0];
+        mx_len = cnt <= k ? 1 : 0;
+
+        l = 0;
+        for (int r = 1; r < n; ++r) {
+            
+            // check the height constraint
+            if (ht[r - 1] % ht[r] != 0) {
+                cnt = 0;
+                l = r;
+            }
+
+            cnt += fr[r];
+
+            while (l < r && cnt > k) {
+                cnt -= fr[l]; ++l;
+            }
+
+            if (cnt <= k)
+                mx_len = max(mx_len, r - l + 1);
+        }
+
+        std::cout << mx_len << "\n";
+    }
+}
